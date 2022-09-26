19e32a7d6947e598a9d77147ba454b6fd24e2e3a
lekhamps <113925327+lekhamps@users.noreply.github.com>
2022-09-26 14:11:31 +0530

    Create Assignment 2.py

---
+++ Assignments/TEAM MEMBER 1- LEKHAPRIYADHARSHINI P/Assignment 2.py
@@ -0,0 +1,25 @@
+import random
+
+def high(t):
+
+    if t>45:
+
+        print("alarm-on")
+
+    else:
+
+        print("alarm-off")
+
+while(1):
+
+    temperature=random.randint(25,100)
+
+    humidity=random.randint(0,100)
+
+    print(temperature)
+
+    print(humidity)
+
+    high(temperature)
+
+print("\n")
